5a6342d5e7566d2637279bd0424f0a7fd00cae5f
Richard L. Jackson <denezt@users.noreply.github.com>
2018-10-13 17:03:53 +0200

    Updating backup container script (#3)

--- backup_container.sh
+++ backup_container.sh
@@ -1,37 +1,65 @@
 #!/bin/bash
+# Written By: Richard Jackson
+# Created: 12 OCT 2018
 #
-#
-# (To do parse input)
 
 option="${1}"
 
-docker ps -a
-read -p "Enter Container ID: " _container_id
-docker ps -a | grep "${_container_id}"
+error(){
+	[ ! -z "${1}" ] && printf "Error: \033[31m${1}\033[0m\n" || \
+	printf "Error: \033[31mUnknown Error Occurred!\033[0m\n"
+	}
 
 save_docker_image() {
-	if [ ! -z "${_container_id}" ];
+	read -p "Enter Container ID: " _container_id
+	__valid_image=$(docker ps -a | grep "${_container_id}")
+	if [ ! -z "${__valid_image}" ];
 	then
-		read -p "Enter a name for container: " _newname
-		if [ ! -z "${_newname}" ];
+		if [ ! -z "${_container_id}" ];
 		then
-			docker commit ${_container_id} ${_newname}
-			docker save ${_newname} > /tmp/${_newname}.tar
+			read -p "Enter a name for container: " _newname
+			if [ ! -z "${_newname}" ];
+			then
+				docker commit ${_container_id} ${_newname}
+				docker save ${_newname} > /tmp/${_newname}.tar
+			fi
+		else
+			printf "Missing container id!\n"
 		fi
 	else
-		printf "Missing container id!\n"
+		error "Invalid image id was given"
 	fi
 	}
 
 load_docker_image(){
-	read -p "Do you really want to load image? [yes|no]" _confirm
-	case ${_confirm} in
-		yes|y) docker load < /tmp/${_newname}.tar;;
-		*) printf "cowardly, exiting from program.";;
-	esac
-}
+	read -p "Enter Backup Image Archive: " _image_backup
+	if [ -e "/tmp/${_image_backup}.tar" ];
+	then
+		read -p "Do you really want to load image? [yes|no]" _confirm
+		case ${_confirm} in
+			yes|y) docker load < /tmp/${_image_backup}.tar;;
+			*) printf "cowardly, exiting from program.";;
+		esac
+	else
+		error "Unable to find archive file!"
+	fi
+	}
+
+validate_docker(){
+	__docker_state=$(docker ps -a | echo $?)
+	echo "${__docker_state}"
+	}
+
+help_menu(){
+	printf "Backup/Load Containers\n"
+	printf "Save Docker Image [ -s, -save, --save ]\n"
+	printf "Load Docker Image [ -l, -load, --load ]\n"
+	}
 
 case $option in
-	-save) save_docker_image;;
-	-load) load_docker_image;;
+	-s|-save|--save) save_docker_image;;
+	-l-load|--load) load_docker_image;;
+	-v|-validate|--validate) validate_docker;;
+	-h|-help|--help) help_menu;;
+	*) error "Missing or invalid parameter was given";;
 esac
